f6ac6d22222cc671f582467296bb4cbbe64571a6
Md-FarhadHossain <freelancerfarhadhossain@gmail.com>
2022-12-05 01:12:21 +0600

    footer added

---
+++ src/HomePage/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <foter>
+        <p className='text-center mb-4 mt-8'>Copyright © 2022 - <span className="font-semibold">Farhad Hossain</span> - devChallenges.io</p>
+    </foter>
+  )
+}
+
+export default Footer
